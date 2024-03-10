6c791af1b039eb080ad5563a92b2d7ff54f0c791
sebastian <stmr.tay@gmail.com>
2024-03-09 19:08:08 -0500

    Created nodes for binary tree

---
+++ tree.js
@@ -0,0 +1,19 @@
+class TreeNode {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+let one = new TreeNode('1')
+let two = new TreeNode('2')
+let three = new TreeNode('3')
+let four = new TreeNode('4')
+let five = new TreeNode('5')
+let six = new TreeNode('6');
+let seven = new TreeNode('7');
+let eight = new TreeNode('8')
+let nine = new TreeNode('9')
+
+module.exports = TreeNode;
