3841c3d503aed88d2eb3fc21b230bec69f6c822f
sebastian <stmr.tay@gmail.com>
2024-03-09 19:23:28 -0500

    Connected nodes to create binary search tree. All left values are less than node and all right values are greater than.

--- tree.js
+++ tree.js
@@ -6,14 +6,24 @@ class TreeNode {
     }
 }
 
-let one = new TreeNode('1')
-let two = new TreeNode('2')
-let three = new TreeNode('3')
-let four = new TreeNode('4')
-let five = new TreeNode('5')
+let one = new TreeNode('1');
+let two = new TreeNode('2');
+let three = new TreeNode('3');
+let four = new TreeNode('4');
+let five = new TreeNode('5');
 let six = new TreeNode('6');
 let seven = new TreeNode('7');
-let eight = new TreeNode('8')
-let nine = new TreeNode('9')
+let eight = new TreeNode('8');
+let nine = new TreeNode('9');
+
+one.right = two;
+three.left = one;
+three.right = four;
+four.right = five;
+seven.left = three;
+seven.right = eight;
+eight.left= six;
+eight.right = nine;
+
 
 module.exports = TreeNode;
