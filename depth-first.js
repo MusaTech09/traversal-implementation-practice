bc47299aa894e5ff49f52a899688e93e1614910c
sebastian <stmr.tay@gmail.com>
2024-03-09 20:39:10 -0500

    Added draft depthFirstTraversal function. Imported BST and Graph.

--- depth-first.js
+++ depth-first.js
@@ -1,2 +1,20 @@
-const TreeNode = require('./tree.js');
+const root = require('./tree.js');
 const graph = require('./graph.js');
+
+const depthFirstTraversal = root => {
+    //Put starting node on a stack
+    let stack = [root];
+
+    //While stack is empty
+    while(stack.length > 0) {
+        //Pop a node and print it
+        let node = stack.pop();
+        console.log(node.val);
+
+        //Put all node's children on top of stack
+        if(node.left) stack.push(node.left);
+        if(node.right) stack.push(node.right);
+    }
+}
+
+depthFirstTraversal(root);
