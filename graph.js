3988629901711a18305542c8d6c2e5dafe6aec18
sebastian <stmr.tay@gmail.com>
2024-03-09 20:01:12 -0500

    Added notes about different vertices and edges in graph implementation

--- graph.js
+++ graph.js
@@ -1,10 +1,12 @@
+//Create Graph
+
 const graph = {
-    A: new Set(['B', 'C', 'E']);
-    B: new Set([]);
-    C: new Set(['B', 'D']);
-    D: new Set([]);
-    E: new Set([A]);
-    F: new Set([E]);
+    A: new Set(['B', 'C', 'E']), //directed B && C; undirected(bidirectional) E
+    B: new Set([]),
+    C: new Set(['B', 'D']),
+    D: new Set([]),
+    E: new Set([A]), //undirected(bidirectional) edge
+    F: new Set([E]), //directed edge
 }
 
 module.exports = graph;
